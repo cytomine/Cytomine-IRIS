92b9f46db321e61e22d7c53b9be3b7aba1136c65
Philipp Kainz <kainz.p@gmail.com>
2014-12-20 19:42:40 +0100

    added error message to missing credentials at connecting to the server

--- grails-app/conf/SecurityFilters.groovy
+++ grails-app/conf/SecurityFilters.groovy
@@ -1,8 +1,13 @@
+import grails.converters.JSON;
+
 import java.net.Authenticator.RequestorType;
 
+import org.json.simple.JSONObject;
 import org.junit.After;
 
+import be.cytomine.apps.iris.Utils
 import be.cytomine.client.Cytomine
+import be.cytomine.client.CytomineException
 
 class SecurityFilters {
 	def springSecurityService
@@ -13,12 +18,19 @@ class SecurityFilters {
 			before = {
 				String publicKey = params.get("publicKey")
 				String privateKey = params.get("privateKey")
-
-				if(publicKey || privateKey) {
+				
+				if(publicKey && privateKey) {
 					// store a new cytomine instance (from the client-JAR)
 					// to the request object (will be available in the controllers etc.)
 					request['cytomine'] = new Cytomine(grailsApplication.config.grails.cytomine.host, publicKey, privateKey, "./");
 					request['cytomine'].setMax(0); //no max
+				}
+				else {
+					response.setStatus(403)
+					JSONObject exObj = new Utils().resolveCytomineException(new CytomineException(403, "You do not have " +
+						" the permissions to access this resource. Invalid credentials."))
+					render exObj as JSON
+					return false
 				}
 			}
 			after = {
