d93c4ee4df5048d8496ef7a5966f2beaf7719c9a
Philipp Kainz <kainz.p@gmail.com>
2015-05-30 18:41:41 +0200

    updated per-user statistics and renamed tabs

--- web-app/controllers/userStatsController.js
+++ web-app/controllers/userStatsController.js
@@ -116,7 +116,23 @@ iris.controller(
 			};
 
 //	fetch the annotation
-	$scope.refreshPage();
+	//$scope.refreshPage();
+
+	// determine the type of the progress bar (color)
+	$scope.type = function(progress) {
+		if (progress < 50){
+			return "danger";
+		}
+		else if (progress >= 50 && progress < 75){
+			return "warning";
+		}
+		else if (progress >= 75 && progress < 95){
+			return "info";
+		}
+		else if (progress >= 95){
+			return "success";
+		}
+	};
 
 //	//////////////////////////////////////////
 //	declare additional methods
